b784025e07c7dcf364d154adfb2c7b4f9ceda02f
Prince-Sameer <51361242+Prince-Sameer@users.noreply.github.com>
2020-07-03 17:41:25 +0530

    Add files via upload

---
+++ webdevelopement/js/pj.js
@@ -0,0 +1,32 @@
+function msg1(){
+  alert('Kaido of the Beasts,renowned as the "Strongest Creature", is the Governor-General of the Beasts Pirates and one of the Yonko ruling over the New World');
+  var a=document.getElementById("frame");
+  a.src="https://www.youtube.com/embed/7Nn9NQ8ilJo?autoplay=1";
+
+}
+function msg2(){
+  alert('Charlotte Linlin, better known as Big Mom, is the captain of the Big Mom Pirates and one of the Yonko ruling over the New World, as the only female member');
+  document.getElementById("frame").src="https://www.youtube.com/embed/Wm76zS-K6Pk?autoplay=1";
+}
+function msg3(){
+    alert('Red-Haired" Shanks, also commonly known as "Red Hair", is the captain of the Red Hair Pirates and a member of the Yonko that rule over the New World. He is also a former member of the legendary Roger Pirates, the only group to successfully conquer the Grand Line, where he started as a pirate as an apprentice');
+  document.getElementById("frame").src="https://www.youtube.com/embed/9zC-nT9Z37Y?autoplay=1";
+
+}
+function msg4(){
+  alert('Marshall D. Teach, most commonly referred to by his epithet "Blackbeard", is the admiral of the Blackbeard Pirates and one of the Yonko.He is also the only known person in history to wield the power of two Devil Fruits');
+  document.getElementById('frame').src="https://www.youtube.com/embed/B7q099zYSG8?autoplay=1";
+}
+function kaido(){
+  var a=document.getElementById("frame");
+  a.src="https://www.youtube.com/embed/7Nn9NQ8ilJo?autoplay=1";
+}
+function bigmom(){
+  document.getElementById("frame").src="https://www.youtube.com/embed/Wm76zS-K6Pk?autoplay=1";
+}
+function shanks(){
+  document.getElementById("frame").src="https://www.youtube.com/embed/9zC-nT9Z37Y?autoplay=1";
+}
+function bb(){
+  document.getElementById('frame').src="https://www.youtube.com/embed/B7q099zYSG8?autoplay=1";
+}
